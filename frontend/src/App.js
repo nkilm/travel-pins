e64d55066de5267fc27fdc1f0b53c79de9293fab
nkilm <nikhilmohitelhs@gmail.com>
2022-08-01 13:30:07 +0530

    map setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import Map from 'react-map-gl';
+
 function App() {
   return (
     <div className="App">
-     
+      <Map
+        initialViewState={{
+          longitude: 78.6569,
+          latitude: 22.9734,
+          zoom: 4
+        }}
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+        style={{ width: "100vw", height: "100vh" }}
+        mapStyle="mapbox://styles/mapbox/dark-v10"
+      />
     </div>
   );
 }
