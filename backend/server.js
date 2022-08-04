d0747b859ba272bd1562ad156f3555f1d7e8ef63
nkilm <nikhilmohitelhs@gmail.com>
2022-08-04 20:16:21 +0530

    dynamic port

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,12 @@ require("dotenv").config();
 const pinRoute = require("./routes/pins");
 const userRoute = require("./routes/users");
 
-const PORT = 9090;
+const PORT = process.env.PORT || 9090;
 const URL = process.env.MONGODB_URL;
 
 const app = express();
 
-// DB Connection
+// Database Connection
 mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log(`MongoDB Connected...`))
     .catch((err) => console.log(err))
@@ -24,8 +24,9 @@ app.use("/api/pins/", pinRoute); // http://localhost:9090/api/pins
 app.use("/api/users/", userRoute);
 
 app.get("/", (req, res) => {
-    res.send("hello");
+    res.json({"status":"Backend server started..."});
 })
+
 app.listen(PORT, () => {
     console.log(`Listening on http://localhost:${PORT}`);
 })
